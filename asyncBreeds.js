8a7006f2c188dd73907bd7ecbebc26313ce7ca4b
Taejin Jung <taejin5314@gmail.com>
2021-06-29 16:58:04 +0000

    Add test cases for cats

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,17 +1,15 @@
-// asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
-  console.log('breedDetailsFromFile: Calling readFile...');
-  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
+const breedDetailsFromFile = function(breed, callback) {
+  // console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`,'utf8', (error, data) => {
+    // console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) callback(data);
+    else callback(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+module.exports = breedDetailsFromFile;
